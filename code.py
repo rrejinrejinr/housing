4efda03582555956f2218d968a2ef4e54e9b0bb2
rrejinrejinr <rrejinrejin987@gmail.com>
2025-09-14 14:47:36 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,4 +1,8 @@
 import  pandas as pd
 from sklearn.linear_model import LinearRegression 
 df=pd.read_csv("Housing.csv")
-target=df[
+target=df["price"]
+feature= df["area","bedrooms"]
+reg=LinearRegression()
+reg.fit(feature,target)
+reg.predict(4,2309)
